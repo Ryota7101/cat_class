a6c923ab373034eb988009e873f3326ffe69f0db
Ryota Suzuki <35439050+Ryota7101@users.noreply.github.com>
2018-01-31 14:13:25 +0900

    Update dataset_factory.py

--- cat_classifir/datasets/dataset_factory.py
+++ cat_classifir/datasets/dataset_factory.py
@@ -1,6 +1,3 @@
-
-"""分類イメージ/ラベルのペアを返すファクトリパターンクラス."""
-
 from __future__ import division
 from __future__ import print_function
 
@@ -13,22 +10,6 @@ datasets_map = {
 
 
 def get_dataset(name, split_name, dataset_dir, file_pattern=None, reader=None):
-  """データセット名が与えられ、split_nameがデータセットを返します。
-
-   Args：
-     name：String、データセットの名前。
-     split_name：電車/テストのスプリット名。
-     dataset_dir：データセットファイルが格納されているディレクトリ。
-     file_pattern：データセットのソースファイルの照合に使用するファイルパターン。
-     reader：tf.ReaderBaseのサブクラスです。 `None`のままにすると、デフォルト
-       各データセットで定義されたリーダーが使用されます。
-
-   戻り値：
-     `Dataset`クラスです。
-
-   発生する：
-     ValueError：データセット `name`が不明な場合。
-  """
   if name not in datasets_map:
     raise ValueError('Name of dataset unknown %s' % name)
   return datasets_map[name].get_split(
